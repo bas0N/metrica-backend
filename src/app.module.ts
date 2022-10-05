bc679ad474815bc70a583548e145045811244f8d
basON <wch.basinski@gmail.com>
2022-10-05 16:20:26 +0200

    Add survey contorler, module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,13 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthService } from './auth/auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SurveyModule } from './survey/survey.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     MongooseModule.forRoot(`${process.env.MONGO_DB_CONNECTION_STRING}`),
+    SurveyModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/db/repositories/survey.repository.ts
+++ src/db/repositories/survey.repository.ts
@@ -4,14 +4,21 @@ import { Model } from 'mongoose';
 import { AddSurveyDto } from 'src/survey/dto/AddSurvey.dto';
 import { SurveyDocument } from '../schemas/survey.schema';
 import { Survey } from '../schemas/survey.schema';
+import { UsersRepository } from './users.repository';
 @Injectable()
 export class SurveyRepository {
   constructor(
     @InjectModel(Survey.name) private surveyModel: Model<SurveyDocument>,
+    private userRepository: UsersRepository,
   ) {}
 
-  async createSurvey(survey: AddSurveyDto): Promise<Survey> {
-    const newSurvey = new this.surveyModel(survey);
-    return newSurvey.save();
+  async createSurvey(email: string): Promise<any> {
+    const user = await this.userRepository.findUser(email);
+    console.log(user);
   }
+
+  //   async createSurvey(survey: AddSurveyDto): Promise<Survey> {
+  //     const newSurvey = new this.surveyModel(survey);
+  //     return newSurvey.save();
+  //   }
 }

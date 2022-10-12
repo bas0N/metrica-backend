4881732990df32017c55536ac535e91b3d26fea7
basON <wch.basinski@gmail.com>
2022-10-12 23:20:37 +0200

    Add survey controllers

--- src/survey/survey.controller.ts
+++ src/survey/survey.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { String } from 'aws-sdk/clients/acm';
 import { AddSurveyDto } from './dto/AddSurvey.dto';
@@ -18,15 +18,32 @@ export class SurveyController {
   }
   //add user verifiaction
   @UseGuards(AuthGuard('jwt'))
-  @Post('getSurveys')
+  @Get('getSurveys')
   getSurveys() {
     return this.surveyService.getSurveys();
   }
   //get survey details
-
+  @UseGuards(AuthGuard('jwt'))
+  @Get('getSurveyDetails')
+  getSurveyDetails() {
+    return this.surveyService.getSurveys();
+  }
   //edit survey
-
+  @UseGuards(AuthGuard('jwt'))
+  @Post('editSurvey')
+  editSurvey() {
+    return this.surveyService.getSurveys();
+  }
   //delete survey
-
+  @UseGuards(AuthGuard('jwt'))
+  @Post('deleteSurvey')
+  deleteSurvey() {
+    return this.surveyService.getSurveys();
+  }
   //change state
+  @UseGuards(AuthGuard('jwt'))
+  @Post('changeSurveystate')
+  changeSurveystate() {
+    return this.surveyService.getSurveys();
+  }
 }

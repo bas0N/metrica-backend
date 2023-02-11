b13031b7470e802f9802138e5b9087081c000eaa
basON <wch.basinski@gmail.com>
2023-02-11 19:24:30 +0100

    dist

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,18 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
+    app.enableCors({
+        origin: [
+            'http://localhost:3002',
+            'http://localhost:3000',
+            'http://localhost:3001',
+            'https://metrica-app.vercel.app',
+            'https://www.metrica-app.vercel.app',
+            'https://metrica-landing.vercel.app/',
+            'https://www.metrica-landing.vercel.app/',
+        ],
+        credentials: true,
+    });
     await app.listen(process.env.PORT || 3001);
 }
 bootstrap();

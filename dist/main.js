a4440e953e99e8a626948d8566c20d41773fbd5c
basON <wch.basinski@gmail.com>
2023-02-11 15:27:57 +0100

    dist

--- dist/main.js
+++ dist/main.js
@@ -4,18 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors({
-        origin: [
-            'http://localhost:3002',
-            'http://localhost:3000',
-            'http://localhost:3001',
-            'https://metrica-app.vercel.app',
-            'https://www.metrica-app.vercel.app',
-            'https://metrica-landing.vercel.app/',
-            'https://www.metrica-landing.vercel.app/',
-        ],
-        credentials: true,
-    });
+    app.enableCors();
     await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3002',
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'https://metrica-app.vercel.app',
-      'https://www.metrica-app.vercel.app',
-      'https://metrica-landing.vercel.app/',
-      'https://www.metrica-landing.vercel.app/',
-    ],
-    credentials: true,
-  });
+  app.enableCors();
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3002',
+  //     'http://localhost:3000',
+  //     'http://localhost:3001',
+  //     'https://metrica-app.vercel.app',
+  //     'https://www.metrica-app.vercel.app',
+  //     'https://metrica-landing.vercel.app/',
+  //     'https://www.metrica-landing.vercel.app/',
+  //   ],
+  //   credentials: true,
+  // });
 
   await app.listen(process.env.PORT || 3001);
 }

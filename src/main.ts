cac38134ff00d24955929b6eceae343e67193727
basON <wch.basinski@gmail.com>
2023-02-11 13:41:05 +0100

    Add cors url

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
       'http://localhost:3002',
       'http://localhost:3000',
       'http://localhost:3001',
+      'https://metrica-app.vercel.app',
     ],
     credentials: true,
   });
